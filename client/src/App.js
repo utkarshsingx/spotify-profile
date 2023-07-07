07f7563b55708b729039f4596e0b4231b1b51171
Utkarsh <hauntedutkarsh@gmail.com>
2023-07-08 03:08:58 +0530

    initialised

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+      const queryString = window.location.search;
+      const urlParams = new URLSearchParams(queryString);
+      const accessToken = urlParams.get('access_token');
+      const refreshToken = urlParams.get('refresh_token');
+
+    
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8080/login"
+         
+        >
+          Login to spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
