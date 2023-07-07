4a23e819f8160c07c36f1f5f1a5e41992ebf0391
Utkarsh <hauntedutkarsh@gmail.com>
2023-07-08 04:19:04 +0530

    Update deps

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import './App.css';
 
 function App() {
   useEffect(() => {
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const accessToken = urlParams.get('access_token');
-      const refreshToken = urlParams.get('refresh_token');
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
 
-    
-  }, [])
+    console.log(accessToken);
+    console.log(refreshToken);
+
+    if (refreshToken) {
+      fetch(`/refresh_token?refresh_token=${refreshToken}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+    }
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
